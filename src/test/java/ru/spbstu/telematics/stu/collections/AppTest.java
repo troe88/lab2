c3fa6e9300e31d0a49192208f9127c9f37e18d08
dmitry <dmitry.trok@gmai.com>
2014-10-22 15:07:50 +0400

    final

--- src/test/java/ru/spbstu/telematics/stu/collections/AppTest.java
+++ src/test/java/ru/spbstu/telematics/stu/collections/AppTest.java
@@ -29,10 +29,77 @@ public class AppTest
     }
 
     /**
-     * Rigourous Test :-)
+     * Rigorous Test :-)
      */
+    
+    public void addToVector(){
+    	System.out.println("addToVector test");
+    	Vector v = new Vector();
+    	v.add("q");
+    	v.add("w");
+    	v.add("3");
+    	assertEquals(3, v.size());
+    }
+ 
+    public void addToVectorByIndex(){
+    	System.out.println("addToVectorByIndex test");
+    	Vector v = new Vector();
+    	v.add("q");
+    	v.add("w");
+    	v.add("3");
+    	v.add("zxc", 1);
+    	assertEquals("zxc", v.get(1));
+    }
+    
+    public void getElemetn(){
+    	System.out.println("getElemetn");
+    	Vector v = new Vector();
+    	v.add("q");
+    	v.add("w");
+    	v.add("3");
+    	assertEquals("q", v.get(0));
+    }
+    
+    public void removeElement(){
+    	System.out.println("removeElement");
+    	Vector v = new Vector();
+    	v.add("q");
+    	v.add("w");
+    	v.add("3");
+    	v.remove(0);
+    	assertEquals(2, v.size());
+    }
+    
+    public void findElement(){
+    	System.out.println("findElement");
+      	Vector v = new Vector();
+    	v.add("q");
+    	v.add("w");
+    	v.add("3");
+    	assertEquals(1, v.indexOf("w"));
+    }
+    
+    public void foreachSycle(){
+    	System.out.println("foreachSycle");
+    	Vector v = new Vector();
+    	v.add("q");
+    	v.add("w");
+    	v.add("3");
+    	String a = new String();
+    	for (Object object : v) {
+			a += object;
+		}
+    	assertEquals("qw3", a);
+    }
+    
     public void testApp()
     {
-        assertTrue( true );
+    	System.out.println("Start tests. Its wrong test implementation, I must rewrite it yet...");
+    	addToVector();
+    	addToVectorByIndex();
+    	getElemetn();
+    	removeElement();
+    	findElement();
+    	foreachSycle();
     }
 }
